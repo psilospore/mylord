30df7847f8726a1fa2296e3b0efb26609f86d3b3
Syed Jafri <sjafri@learningobjects.com>
2017-10-06 15:54:11 -0400

    Allowing a user to specify either jest or karma as a test runner. Added placeholder jestServer.

---
+++ client/jestServer.js
@@ -0,0 +1,49 @@
+var path = require('path');
+var argv = require('yargs').argv;
+
+
+var Karma = require(argv.module);
+
+var testing = false;
+
+function unpause(){
+  process.send({ type: 'unpause' });
+}
+
+function onSuccess(){
+  // TODO Placeholder copied from karamaServer.js
+  unpause();
+  if(!testing) {
+    testing = true;
+    console.log('Running tests...');
+    new Jest.Server({
+      configFile: path.resolve(argv.config),
+      colors: true
+    }, function(exitCode){
+      testing = false;
+    }).start();
+  } else {
+    //todo: how to cancel?
+  }
+}
+
+function onInvalid() {
+  unpause();
+}
+
+function onStart() {
+  process.send({
+    type: 'pause',
+    value: 'Waiting for build...'
+  });
+}
+
+process.on('message', message => {
+  if(message.value.message === 'build:done' && message.value.success === true){
+    onSuccess();
+  } else if(message.value.message === 'build:done') {
+    onInvalid();
+  } else {
+    onStart();
+  }
+});
